943f1a305b3c9f0d9c3036b94f3c046d58685394
francisco henrique milagres borboleta de lima <francisco.lima@urucuia.grad>
2015-11-18 17:19:00 -0200

    Adicionando alteracoes do Leandro

--- app/src/main/java/ninegroup/ninegame/MainActivity.java
+++ app/src/main/java/ninegroup/ninegame/MainActivity.java
@@ -3,11 +3,10 @@ package ninegroup.ninegame;
 import android.app.Activity;
 import android.content.Context;
 import android.content.Intent;
+import android.graphics.Point;
 import android.os.Bundle;
-import android.util.Log;
 import android.util.TypedValue;
 import android.view.View;
-import android.widget.Gallery;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.RelativeLayout;
@@ -17,23 +16,53 @@ public class MainActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        Point size = new Point();
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        montarTabuleiro();
+        getWindowManager().getDefaultDisplay().getSize(size);
+        montarTabuleiro(size.x, size.y);
     }
 
-    protected void montarTabuleiro(){
+    protected void montarTabuleiro(int width, int height){
         RelativeLayout rl = (RelativeLayout)findViewById(R.id.meuLayout);
+        float linha[] = new float[5];
+        float coluna[] = new float[3];
 
-        CasaView casa_1 = new CasaView(this, (float)100.0, (float)100.0);
-        CasaView casa_2 = new CasaView(this, (float)400.0, (float)100.0);
-        CasaView casa_3 = new CasaView(this, (float)700.0, (float)100.0);
-        CasaView casa_4 = new CasaView(this, (float)1000.0, (float)100.0);
+        linha[0] = 0.0f;
+        linha[1] = 0.2f;
+        linha[2] = 0.4f;
+        linha[3] = 0.6f;
+        linha[4] = 0.8f;
+
+        coluna[0] = 0.0f;
+        coluna[1] = 0.3f;
+        coluna[2] = 0.6f;
+
+        CasaView casa_1 = new CasaView(this, linha[0]*height, coluna[0]*width);
+        CasaView casa_2 = new CasaView(this, linha[1]*height, coluna[0]*width);
+        CasaView casa_3 = new CasaView(this, linha[2]*height, coluna[0]*width);
+        CasaView casa_4 = new CasaView(this, linha[4]*height, coluna[0]*width);
+        CasaView casa_5 = new CasaView(this, linha[0]*height, coluna[1]*width);
+        CasaView casa_6 = new CasaView(this, linha[2]*height, coluna[1]*width);
+        CasaView casa_7 = new CasaView(this, linha[4]*height, coluna[1]*width);
+        CasaView casa_8 = new CasaView(this, linha[0]*height, coluna[2]*width);
+        CasaView casa_9 = new CasaView(this, linha[1]*height, coluna[2]*width);
+        CasaView casa_10 = new CasaView(this, linha[2]*height, coluna[2]*width);
+        CasaView casa_11 = new CasaView(this, linha[3]*height, coluna[2]*width);
+        CasaView casa_12 = new CasaView(this, linha[4]*height, coluna[2]*width);
 
         rl.addView(casa_1);
         rl.addView(casa_2);
         rl.addView(casa_3);
         rl.addView(casa_4);
+        rl.addView(casa_5);
+        rl.addView(casa_6);
+        rl.addView(casa_7);
+        rl.addView(casa_8);
+        rl.addView(casa_9);
+        rl.addView(casa_10);
+        rl.addView(casa_11);
+        rl.addView(casa_12);
     }
     
 }
@@ -42,6 +71,7 @@ class CasaView extends ImageView {
 
     public CasaView(Context context, float posicaoY, float posicaoX){
         super(context);
+        this.setX(posicaoX);
         this.setY(posicaoY);
         this.setClickable(true);
         this.setImageResource(R.drawable.pergunta_atual);
@@ -51,13 +81,13 @@ class CasaView extends ImageView {
         LinearLayout.LayoutParams parms = new LinearLayout.LayoutParams(tamanho, tamanho);
         this.setLayoutParams(parms);
 
-        this.setOnClickListener(new View.OnClickListener(){
+        this.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                trocarImagem();
-
                 Intent intent = new Intent(v.getContext(), QuestionActivity.class);
                 v.getContext().startActivity(intent);
+
+                trocarImagem();
             }
         });
     }
